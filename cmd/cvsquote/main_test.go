3bbc17324e6e26250f19b8d53d0afab546ab2a9b
Adam Gordon Bell <adam@corecursive.com>
2021-12-01 10:46:06 -0500

    map version

--- cmd/cvsquote/main_test.go
+++ cmd/cvsquote/main_test.go
@@ -59,7 +59,7 @@ func apply(data []byte, f mapper) []byte {
 // }
 
 func TestIdentity2(t *testing.T) {
-	c := quick.Config{MaxCount: 10000,
+	c := quick.Config{MaxCount: 1000,
 		Values: func(values []reflect.Value, r *rand.Rand) {
 			values[0] = reflect.ValueOf(randCSV(r))
 		}}
@@ -104,4 +104,105 @@ func doesIndentityHold(in string) bool {
 	substituted := apply([]byte(in), substitute)
 	restored := string([]byte(apply(substituted, restore)))
 	return in == restored
+}
+
+type quotedByte struct {
+	b       byte
+	inQuote bool
+}
+
+type quotedString struct {
+	delimiter byte
+	quotechar byte
+	recordsep byte
+}
+
+func Map(vs []byte, f func(byte) byte) []byte {
+	vsm := make([]byte, len(vs))
+	for i, v := range vs {
+		vsm[i] = f(v)
+	}
+	return vsm
+}
+
+//this is not good, should be unfold
+func Map1(vs []byte, f func(quotedByte) quotedByte) []byte {
+	vsm := make([]byte, len(vs))
+	inQuote := false
+	for i, v := range vs {
+		qbyte := quotedByte{b: v, inQuote: inQuote}
+		v1 := f(qbyte)
+		vsm[i] = v1.b
+		inQuote = v1.inQuote
+	}
+	return vsm
+}
+
+func (q quotedString) toUnquoted(b byte) byte {
+	if b == delimiterNonprintingByte {
+		return q.delimiter
+	} else if b == recordsepNonprintingByte {
+		return q.recordsep
+	} else {
+		return b
+	}
+}
+
+func (q quotedString) toQuoted(b quotedByte) quotedByte {
+	if b.b == q.quotechar && b.inQuote { //unquote
+		return quotedByte{b: b.b, inQuote: false}
+	} else if b.b == q.quotechar { //quote
+		return quotedByte{b: b.b, inQuote: true}
+	} else if b.b == q.recordsep && b.inQuote { //delimited newline
+		return quotedByte{b: recordsepNonprintingByte, inQuote: b.inQuote}
+	} else if b.b == q.delimiter && b.inQuote { //delimited comma
+		return quotedByte{b: delimiterNonprintingByte, inQuote: b.inQuote}
+	} else {
+		return b
+	}
+}
+
+func TestRestore2(t *testing.T) {
+	q := quotedString{
+		delimiter: ',',
+		recordsep: '\n',
+		quotechar: '"',
+	}
+	for _, tt := range tests {
+		in := string(Map([]byte(tt.out), q.toUnquoted))
+		assert.Equal(t, tt.in, in, "input and output should match")
+	}
+}
+
+func TestSubstitute2(t *testing.T) {
+	q := quotedString{
+		delimiter: ',',
+		recordsep: '\n',
+		quotechar: '"',
+	}
+	for _, tt := range tests {
+		out := string(Map1([]byte(tt.in), q.toQuoted))
+		assert.Equal(t, tt.out, out, "input and output should match")
+	}
+}
+
+func doesIndentityHold2(in string) bool {
+	q := quotedString{
+		delimiter: ',',
+		recordsep: '\n',
+		quotechar: '"',
+	}
+	out := string(Map1([]byte(in), q.toQuoted))
+	restored := string(Map([]byte(out), q.toUnquoted))
+	return in == restored
+}
+
+func TestIdentity3(t *testing.T) {
+	c := quick.Config{MaxCount: 10000,
+		Values: func(values []reflect.Value, r *rand.Rand) {
+			values[0] = reflect.ValueOf(randCSV(r))
+		}}
+	if err := quick.Check(doesIndentityHold2, &c); err != nil {
+		t.Error(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -4,5 +4,4 @@ go 1.15
 
 require (
 	github.com/stretchr/testify v1.7.0
-	github.com/traefik/yaegi v0.11.0 // indirect
 )
